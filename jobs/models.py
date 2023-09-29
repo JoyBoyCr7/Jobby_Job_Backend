8711dfeaebc0a89e580a0af8f2b5b3a1a81bbf61
JoyBoyCr7 <Rnyongkah@gmail.com>
2023-09-28 19:23:59 -0500

    models

--- jobs/models.py
+++ jobs/models.py
@@ -4,8 +4,8 @@ from django.db import models
 class Job(models.Model):
     role = models.CharField(max_length=100)
     location = models.CharField(max_length=100)
-    brand_image = models.CharField(max_length=100)
-    description = models.CharField(max_length=200)
+    brand_image = models.CharField(max_length=300)
+    description = models.CharField(max_length=300)
     interest_level = models.IntegerField()
     application_date = models.DateField()
 
