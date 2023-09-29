d9bc8d7c81981078c0f6e3f515df0efc56cc8428
JoyBoyCr7 <Rnyongkah@gmail.com>
2023-09-28 19:47:58 -0500

    models

--- jobs/models.py
+++ jobs/models.py
@@ -5,7 +5,7 @@ class Job(models.Model):
     role = models.CharField(max_length=100)
     location = models.CharField(max_length=100)
     brand_image = models.CharField(max_length=300)
-    description = models.CharField(max_length=300)
+    description = models.TextField(max_length=1000)
     interest_level = models.IntegerField()
     application_date = models.DateField()
 
